d7c233fe329eca3d5919295bd09a5b30d1417831
Keana Cobarde <keanacobarde@gmail.com>
2024-01-15 21:13:31 -0600

    Add project files.

---
+++ WebAPITestMySQL/Data/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPITestMySQL.Models;
+
+namespace WebAPITestMySQL.Data
+{
+    public class LibraryContext : DbContext
+    {
+     public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        { 
+            
+        }
+
+        public DbSet<Authors> Authors { get; set; }
+    }
+}
---
+++ WebAPITestMySQL/Models/Authors.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPITestMySQL.Models
+{
+    public class Authors
+    {
+        public int AuthorId { get; set; }
+        public string AuthorFirstName { get; set; }
+        public string AuthorLastName { get; set; }
+        public string AuthorNationality { get; set; }
+        
+    }
+}
